eb3b255a62a6f0f6c094be00f850663699409a19
Victor Vieira <victorvieira302@gmail.com>
2018-04-21 10:29:57 -0300

    Create basic structure to project
    Signed-off-by: Victor Vieira <victorvieira302@gmail.com>

---
+++ app/src/main/java/com/vctapps/santanderchallenge/FormActivity.kt
@@ -0,0 +1,12 @@
+package com.vctapps.santanderchallenge
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FormActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form)
+    }
+}
