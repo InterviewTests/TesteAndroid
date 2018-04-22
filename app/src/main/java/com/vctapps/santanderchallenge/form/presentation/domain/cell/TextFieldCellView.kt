16e4454c944a7fb7e0e5adbd6605a8ce8a606f5b
Victor Vieira <victorvieira302@gmail.com>
2018-04-22 12:14:56 -0300

    layout improvements on text field cell
    Signed-off-by: Victor Vieira <victorvieira302@gmail.com>

--- app/src/main/java/com/vctapps/santanderchallenge/form/presentation/domain/cell/TextFieldCellView.kt
+++ app/src/main/java/com/vctapps/santanderchallenge/form/presentation/domain/cell/TextFieldCellView.kt
@@ -3,10 +3,15 @@ package com.vctapps.santanderchallenge.form.presentation.domain.cell
 import android.content.res.ColorStateList
 import android.graphics.Color
 import android.support.design.widget.TextInputEditText
+import android.support.v4.content.ContextCompat
+import android.support.v4.content.res.ResourcesCompat
 import android.support.v4.view.ViewCompat
+import android.text.InputType
 import android.view.ViewGroup
 import com.vctapps.santanderchallenge.R
+import com.vctapps.santanderchallenge.form.domain.cell.EmailFieldCell
 import com.vctapps.santanderchallenge.form.domain.cell.FieldCell
+import com.vctapps.santanderchallenge.form.domain.cell.TelNumberFieldCell
 import com.vctapps.santanderchallenge.form.domain.cell.TextFieldCell
 import com.vctapps.santanderchallenge.form.presentation.domain.cell.base.FieldCellView
 import kotlinx.android.synthetic.main.text_field_cell_view.view.*
@@ -40,5 +45,18 @@ class TextFieldCellView(private val textFieldCell: FieldCell,
         inflateLayout(R.layout.text_field_cell_view)
 
         editText = this.view.editTextTextCell
+
+        val textInputLayout = this.view.textInputLayoutTextCell
+
+        textInputLayout.hint = textFieldCell.message
+
+        when(textFieldCell){
+            is EmailFieldCell -> editText.inputType = InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
+            is TelNumberFieldCell -> editText.inputType = InputType.TYPE_CLASS_PHONE
+            is TextFieldCell -> editText.inputType = InputType.TYPE_CLASS_TEXT
+        }
+
+        val colorStateList = ColorStateList.valueOf(ContextCompat.getColor(rootView.context, R.color.gray_600))
+        ViewCompat.setBackgroundTintList(editText, colorStateList)
     }
 }
