6b789faf73e7a30784f6b122051fda45a0b4da1b
Victor Vieira <victorvieira302@gmail.com>
2018-04-22 14:51:46 -0300

    improvements on dashboard tab layout
    Signed-off-by: Victor Vieira <victorvieira302@gmail.com>

--- app/src/main/java/com/vctapps/santanderchallenge/dashboard/DashboardActivity.kt
+++ app/src/main/java/com/vctapps/santanderchallenge/dashboard/DashboardActivity.kt
@@ -1,7 +1,7 @@
 package com.vctapps.santanderchallenge.dashboard
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import com.vctapps.santanderchallenge.R
 import kotlinx.android.synthetic.main.activity_dashboard.*
 
@@ -9,13 +9,21 @@ class DashboardActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_dashboard)
 
+        setUpViewPager()
+
+        setUpTabLayout()
+    }
+
+    private fun setUpViewPager() {
         val adapterViewPager = DashboardPageAdapter(supportFragmentManager)
 
         viewPagerDashboard.adapter = adapterViewPager
+    }
 
+    private fun setUpTabLayout() {
         tabLayoutDashboard.setupWithViewPager(viewPagerDashboard)
-
     }
 }
