37688acdfb12e0ca84e23dd44254b765decc03a9
Mellina <mellinacerqueira@email.com>
2018-05-04 17:24:15 -0300

    adicionado pacote com classes adapters

---
+++ TesteAndroidSantander/app/src/main/java/com/adapters/Info.java
@@ -0,0 +1,20 @@
+package com.adapters;
+
+public class Info {
+
+    private String name;
+    private String data;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public Info(String name, String data) {
+        this.name = name;
+        this.data = data;
+    }
+}
