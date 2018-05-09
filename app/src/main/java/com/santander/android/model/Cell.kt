b38050eea240ae4febb1b1d207f499582e187bb6
s-000001 <s-000001@protonmail.com>
2018-05-09 20:34:02 -0300

    Models

---
+++ app/src/main/java/com/santander/android/model/Cell.kt
@@ -0,0 +1,44 @@
+package com.santander.android.model
+
+import java.io.Serializable
+
+data class Cell(
+        var id: Int? = null,
+        var type: Int? = null,
+        var Message: String? = null,
+        var typefield: String? = null,
+        var hidden: Boolean = false,
+        var topSpacing: Int? = null,
+        var show: Int? = null,
+        var required: Boolean = false
+): Serializable {
+
+    enum class Type(val value: Int) {
+
+        Field(1),
+        Text(2),
+        Image(3),
+        Checkbox(4),
+        Send(5);
+
+        companion object {
+            fun from(findValue: Int): Type = Type.values().first { it.value == findValue }
+        }
+
+    }
+
+    enum class TypeField(val value: Int) {
+
+        Text(1),
+        TelNumber(2),
+        Email(3);
+
+        companion object {
+            // Fixes Broken "telNumber" type field.
+            fun from(findValue: String): TypeField = TypeField.values().first { it.value == fixBrokenJson(findValue) }
+            private fun fixBrokenJson(brokenValue: String): Int = brokenValue.toIntOrNull()?: 2
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/santander/android/model/Fund.kt
@@ -0,0 +1,40 @@
+package com.santander.android.model
+
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
+data class Fund(
+        var screen: Fund.Screen? = null
+): Serializable {
+
+    data class Screen(
+            var title: String? = null,
+            var fundName: String? = null,
+            var whatIs: String? = null,
+            var definition: String? = null,
+            var riskTitle: String? = null,
+            var risk: Int? = null,
+            var infoTitle: String? = null,
+            var moreInfo: Investment? = null,
+            var info: List<Info> = ArrayList(),
+            var downInfo: List<Info> = ArrayList()
+    ) : Serializable
+
+    data class Investment(
+            var month: Rate? = null,
+            var year: Rate? = null,
+            @SerializedName(value = "12months")
+            var twelveMonths: Rate? = null
+    ) : Serializable
+
+    data class Rate(
+            var fund: Double? = null,
+            var CDI: Double? = null
+    ) : Serializable
+
+    data class Info(
+            var name: String? = null,
+            var data: String? = null
+    ) : Serializable
+
+}
