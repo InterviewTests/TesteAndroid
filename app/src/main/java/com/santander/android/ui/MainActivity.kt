dd861c6956bb66cc9790212ca4c1a5fd2f5d3756
s-000001 <s-000001@protonmail.com>
2018-05-09 18:55:54 -0300

    Project Created

---
+++ app/src/main/java/com/santander/android/ui/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.santander.android.ui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.santander.android.R
+
+class MainActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+
+    }
+
+}
