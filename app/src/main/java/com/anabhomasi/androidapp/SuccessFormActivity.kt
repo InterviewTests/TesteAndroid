e6c085a1985ea1ed7682738ea12f6d0dfb4292ae
Unknown <anathomasi@hotmail.com>
2018-05-13 19:52:32 -0300

    Created Success Form

---
+++ app/src/main/java/com/anabhomasi/androidapp/SuccessFormActivity.kt
@@ -0,0 +1,12 @@
+package com.anabhomasi.androidapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SuccessFormActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_success_form)
+    }
+}
