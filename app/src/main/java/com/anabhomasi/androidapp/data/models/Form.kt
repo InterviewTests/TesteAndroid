c9db925cbc3843be3afdcf47cf09a30a70e8e34c
Ana Thomasi <anathomasi@hotmail.com>
2018-05-26 10:26:16 -0300

    Added Enum to Form model

--- app/src/main/java/com/anabhomasi/androidapp/data/models/Form.kt
+++ app/src/main/java/com/anabhomasi/androidapp/data/models/Form.kt
@@ -16,4 +16,25 @@ object Form {
         val show: Any,
         val required: Boolean
     )
+
+    enum class Type(val code: Int) {
+        FIELD (1),
+        TEXT (2),
+        IMAGE(3),
+        CHECKBOX(4),
+        SEND(5);
+
+        companion object {
+            fun from (value: Int) : Type? = values().find{ it.code == value }
+        }
+    }
+    enum class TypeField (val code: Int){
+        TEXT(1),
+        TELNUMBER(2),
+        EMAIL(3);
+
+        companion object {
+            fun from (value: Int) : TypeField? = values().find{ it.code == value }
+        }
+    }
 }
