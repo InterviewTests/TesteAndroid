a9141e9589bbb79481d323b2cec84f94e64ef9c1
Ana Thomasi <anathomasi@hotmail.com>
2018-05-26 17:27:52 -0300

    Added dynamic form logic and layout

--- app/src/main/java/com/anabhomasi/androidapp/views/adapters/FormAdapter.kt
+++ app/src/main/java/com/anabhomasi/androidapp/views/adapters/FormAdapter.kt
@@ -1,35 +1,262 @@
 package com.anabhomasi.androidapp.views.adapters
 
+import android.animation.AnimatorInflater
+import android.animation.AnimatorSet
 import android.annotation.SuppressLint
-import android.graphics.Color
+import android.graphics.drawable.TransitionDrawable
+import android.support.design.widget.TextInputEditText
+import android.support.design.widget.TextInputLayout
 import android.support.v7.widget.RecyclerView
+import android.text.Editable
+import android.text.InputFilter
+import android.text.InputType
+import android.text.TextWatcher
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.widget.CheckBox
 import com.anabhomasi.androidapp.App
 import com.anabhomasi.androidapp.R
-import kotlinx.android.synthetic.main.layout_info_row.view.*
+import com.anabhomasi.androidapp.data.models.Form
+import com.anabhomasi.androidapp.views.fragments.DynamicFormFragment
+import kotlinx.android.synthetic.main.layout_dynamic_form_row.view.*
 
-class FormAdapter: RecyclerView.Adapter<FormViewHolder>() {
+class FormAdapter(val fragment: DynamicFormFragment): RecyclerView.Adapter<FormViewHolder>() {
+
+    private lateinit var mParentView : View
+    private var topSpacingTotal = 0.0
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FormViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
-        val cellFromRow = layoutInflater.inflate(R.layout.layout_info_row, parent, false)
-        return FormViewHolder(cellFromRow)
-    }
+
+        mParentView = parent
+
+        val rowView = when (viewType) {
+            Form.TYPE_FIELD -> layoutInflater.inflate(R.layout.layout_dynamic_form_field, parent, false)
+            Form.TYPE_IMAGE -> layoutInflater.inflate(R.layout.layout_dynamic_form_image, parent, false)
+            Form.TYPE_CHECKBOX -> layoutInflater.inflate(R.layout.layout_dynamic_form_checkbox, parent, false)
+            Form.TYPE_SEND -> layoutInflater.inflate(R.layout.layout_dynamic_form_button, parent, false)
+            Form.TYPE_TEXT -> layoutInflater.inflate(R.layout.layout_dynamic_form_text, parent, false)
+            else -> layoutInflater.inflate(R.layout.layout_dynamic_form_text, parent, false)
+        }
+
+        return FormViewHolder(rowView)
+}
 
     override fun getItemCount(): Int {
         return App.getInstance().form.cells.size
     }
 
-    @SuppressLint("SetTextI18n")
+    override fun getItemViewType(position: Int): Int {
+        return App.getInstance().form.cells[position].type
+    }
+
+    @SuppressLint("SetTextI18n", "ClickableViewAccessibility")
     override fun onBindViewHolder(holder: FormViewHolder, position: Int) {
         holder.view.apply {
+            with (App.getInstance().form.cells[position]){
+                when (getItemViewType(position)){
+                    Form.TYPE_FIELD -> {
+                        dynamicTextInputLayout.apply {
+                            tag = App.getInstance().form.cells[position].id
+                            visibility = if (hidden) View.GONE else View.VISIBLE
+                            hint = message
+                            editText?.inputType = when (Form.TypeField.from(typefield?.toUpperCase())?.code) {
+                                        Form.TypeField.TEXT.code -> InputType.TYPE_CLASS_TEXT
+                                        Form.TypeField.EMAIL.code -> InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
+                                        Form.TypeField.TELNUMBER.code -> {
+                                            editText?.filters = arrayOf(InputFilter.LengthFilter(11))
+                                            InputType.TYPE_CLASS_PHONE
+                                        }
+                                        else -> InputType.TYPE_CLASS_TEXT
+                            }
 
+                            (this.editText as? TextInputEditText)?.configureFieldValidation()
+
+                            this.setTopSpacing(topSpacing)
+
+                            if (show != null) {
+                                this.setShowClickListener(show)
+                            }
+                        }
+                    }
+                    Form.TYPE_TEXT -> {
+                        dynamicTextView.apply {
+                            tag = App.getInstance().form.cells[position].id
+                            visibility = if (hidden) View.GONE else View.VISIBLE
+                            text = message
+
+                            this.setTopSpacing(topSpacing)
+
+                            if (show != null) {
+                                this.setShowClickListener(show)
+                            }
+                        }
+                    }
+                    Form.TYPE_IMAGE -> {
+                        dynamicImageView.apply {
+                            tag = App.getInstance().form.cells[position].id
+                            visibility = if (hidden) View.GONE else View.VISIBLE
+
+                            this.setTopSpacing(topSpacing)
+
+                            if (show != null) {
+                                this.setShowClickListener(show)
+                            }
+                        }
+                    }
+                    Form.TYPE_CHECKBOX -> {
+                        dynamicCheckBox.apply {
+                            tag = App.getInstance().form.cells[position].id
+                            visibility = if (hidden) View.GONE else View.VISIBLE
+                            text = message
+
+                            this.setTopSpacing(topSpacing)
+
+                            if (show != null) {
+                                (this as CheckBox).setShowClickListener(show)
+                            }
+                        }
+                    }
+                    Form.TYPE_SEND -> {
+                        dynamicButton.apply {
+                            tag = App.getInstance().form.cells[position].id
+                            visibility = if (hidden) View.GONE else View.VISIBLE
+                            text = message
+
+                            this.setTopSpacing(topSpacing)
+
+                            this.setOnTouchListener { button, event ->
+                                when (event.action) {
+                                    MotionEvent.ACTION_DOWN -> {
+                                        val reducer = AnimatorInflater.loadAnimator(context, R.animator.reduce_size) as AnimatorSet
+                                        reducer.setTarget(button)
+                                        val drawable = this.background as TransitionDrawable
+
+                                        reducer.start()
+                                        drawable.startTransition(300)
+                                    }
+                                    MotionEvent.ACTION_UP -> {
+                                        val regainer = AnimatorInflater.loadAnimator(context, R.animator.regain_size) as AnimatorSet
+                                        regainer.setTarget(button)
+                                        val drawable = this.background as TransitionDrawable
+
+                                        regainer.start()
+                                        drawable.reverseTransition(300)
+
+                                        onSendButtonTouched()
+                                    }
+                                }
+                                true
+                            }
+                        }
+                    }
+                    else -> { }
+                }
+            }
         }
+    }
+
+    private fun onSendButtonTouched() {
+        fragment.onButtonPressed(PageAdapter.SUCCESS_FORM_PAGE)
+    }
+
+    private fun TextInputEditText.configureFieldValidation() {
+        val textInputEditText = this
+
+        this.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable) {
+                fragment.validateField(textInputEditText, s)
+            }
+        })
+        this.setOnEditorActionListener { v, actionId, _ ->
+            if(actionId == EditorInfo.IME_ACTION_DONE || actionId == EditorInfo.IME_ACTION_NEXT){
+
+                if (textInputEditText.inputType == InputType.TYPE_CLASS_PHONE) {
+                    //New max length
+                    (v as TextInputEditText).filters = arrayOf(InputFilter.LengthFilter(15))
+                    //Format number
+                    v.setText(v.text.toString().maskPhone())
+                }
+
+                fragment.validateField(textInputEditText)
+
+                true
+            } else {
+                false
+            }
+        }
+        this.onFocusChangeListener = View.OnFocusChangeListener { v, hasFocus ->
+            if (!hasFocus) {
+                if (textInputEditText.inputType == InputType.TYPE_CLASS_PHONE) {
+                    //New max length
+                    (v as TextInputEditText).filters = arrayOf(InputFilter.LengthFilter(15))
+                    //Format number
+                    v.setText(v.text.toString().maskPhone())
+                }
+
+                fragment.validateField(textInputEditText)
+            } else {
+                (v.parent.parent as TextInputLayout).error = null
+            }
+        }
+    }
+
+    private fun String.maskPhone ():String{
+        val cleanPhone = this.unmaskPhone()
+
+        return if (cleanPhone.length >= 10){
+            val ddd = cleanPhone.substring(0..1)
+            val phoneLeft = cleanPhone.substring(2..cleanPhone.length-5)
+            val phoneRight = cleanPhone.substring(cleanPhone.length-4 until cleanPhone.length)
+            "($ddd) $phoneLeft-$phoneRight"
+        }
+        else this
+    }
+    private fun String.unmaskPhone():String{
+        return this
+                .replace("(", "")
+                .replace(")", "")
+                .replace(" ", "")
+                .replace("-", "")
+    }
+
+    private fun View.setShowClickListener(show: Int){
+        val viewToBeShown = mParentView.findViewWithTag<View>(show)
+
+        if (this is CheckBox) this.setOnCheckedChangeListener { _, isChecked ->
+            viewToBeShown?.visibility =
+                        if (isChecked) View.VISIBLE
+                        else View.GONE
+        } else {
+            this.setOnClickListener {
+                viewToBeShown?.visibility =
+                        if (viewToBeShown.visibility == View.VISIBLE) View.GONE
+                        else View.VISIBLE
+
+            }
+        }
+
+        viewToBeShown?.setTopSpacing(App.getInstance().form.cells[show].topSpacing)
+    }
+
+    private fun View.setTopSpacing(topSpacing: Double){
+        //if (this.visibility == View.VISIBLE){
+            topSpacingTotal += topSpacing
+            val marginLayoutParams = this.layoutParams as ViewGroup.MarginLayoutParams
+            marginLayoutParams.setMargins(33, topSpacing.toInt(), 33, 0)
+            this.layoutParams = marginLayoutParams
+        //} else {
+        //   this.layoutParams = RecyclerView.LayoutParams(0, 0)
+        //}
+
     }
 }
 
-class FormViewHolder(val view: View): RecyclerView.ViewHolder(view){
-
-}
+class FormViewHolder(val view: View): RecyclerView.ViewHolder(view)
--- app/src/main/java/com/anabhomasi/androidapp/views/fragments/DynamicFormFragment.kt
+++ app/src/main/java/com/anabhomasi/androidapp/views/fragments/DynamicFormFragment.kt
@@ -1,52 +1,123 @@
 package com.anabhomasi.androidapp.views.fragments
 
 import android.content.Context
-import android.net.Uri
+import android.content.res.ColorStateList
 import android.os.Bundle
+import android.support.design.widget.TextInputEditText
+import android.support.design.widget.TextInputLayout
 import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
+import android.support.v4.view.ViewCompat
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.text.Editable
+import android.text.InputType
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
+import com.anabhomasi.androidapp.App
 import com.anabhomasi.androidapp.R
+import com.anabhomasi.androidapp.data.models.Form
+import com.anabhomasi.androidapp.views.adapters.FormAdapter
+import kotlinx.android.synthetic.main.fragment_dynamic_form.*
+import java.util.regex.Pattern
 
+private const val PHONE_REGEX = "^\\((\\d{2})\\)\\s(\\d{4,5}\\-\\d{4})\$"
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [DynamicFormFragment.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [DynamicFormFragment.newInstance] factory method to
- * create an instance of this fragment.
- *
- */
 class DynamicFormFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-    private var listener: OnFragmentInteractionListener? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    var listener: OnFragmentInteractionListener? = null
+    private var shouldContinue = true
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dynamic_form, container, false)
+        val view = inflater.inflate(R.layout.fragment_dynamic_form, container, false)
+
+        setFormRecyclerView(view)
+
+        return view
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    fun onButtonPressed(uri: Uri) {
-        listener?.onFragmentInteraction(uri)
+    private fun setFormRecyclerView(view: View?) {
+        val recyclerView = view?.findViewById<RecyclerView>(R.id.formRecyclerView)
+        recyclerView?.layoutManager = LinearLayoutManager(context)
+        recyclerView?.adapter = FormAdapter(this)
+    }
+
+    fun onButtonPressed(page: Int) {
+        validateAllFields()
+
+        if (shouldContinue) {
+            clearAllFields()
+            listener?.onFragmentInteraction(page)
+        }
+    }
+
+    fun validateField(editText: TextInputEditText, s: Editable? = null){
+        val txLayout = editText.parent.parent as TextInputLayout
+        val content = s ?: editText.text
+
+        val emailValidation =
+                if (editText.inputType == InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS)
+                    Patterns.EMAIL_ADDRESS.matcher(content).matches()
+                else
+                    true
+
+        val phoneValidation =
+                if (editText.inputType == InputType.TYPE_CLASS_PHONE)
+                    Pattern.compile(PHONE_REGEX).matcher(content).matches()
+                else
+                    true
+
+        if (content.isNotBlank() && emailValidation && phoneValidation){
+            txLayout.error = null
+            val colorStateList = ColorStateList.valueOf(ContextCompat.getColor(editText.context, R.color.validEditText))
+            ViewCompat.setBackgroundTintList(editText, colorStateList)
+        } else {
+            shouldContinue = false
+            val hint = txLayout.hint
+            txLayout.error = "$hint Inválido"
+        }
+    }
+
+
+    private fun validateAllFields() {
+        shouldContinue = true
+
+        App.getInstance().form.cells.forEach {cell ->
+            if (cell.required){
+                val itemView = when (cell.type){
+                    Form.TYPE_FIELD -> formRecyclerView.findViewWithTag<TextInputLayout>(cell.id)
+                    //Form.TYPE_IMAGE -> formRecyclerView.findViewWithTag<ImageView>(cell.id)
+                    //Form.TYPE_CHECKBOX -> formRecyclerView.findViewWithTag<CheckBox>(cell.id)
+                    //Form.TYPE_SEND -> formRecyclerView.findViewWithTag<Button>(cell.id)
+                    //Form.TYPE_TEXT -> formRecyclerView.findViewWithTag<TextView>(cell.id)
+                    else -> null
+
+                }
+
+                if (itemView?.visibility == View.VISIBLE /*&& itemView is TextInputLayout*/){
+                    validateField(itemView.editText as TextInputEditText)
+                }
+            }
+        }
+    }
+
+    private fun clearAllFields() {
+
+        for (i in 0 until formRecyclerView.childCount){
+            val itemView = formRecyclerView.getChildAt(i)
+            if (itemView is TextInputLayout){
+                itemView.editText?.text?.clear()
+                itemView.error = null
+            }
+            else if (itemView is CheckBox){
+                itemView.isChecked = false
+            }
+        }
     }
 
     override fun onAttach(context: Context) {
@@ -63,39 +134,13 @@ class DynamicFormFragment : Fragment() {
         listener = null
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson [Communicating with Other Fragments]
-     * (http://developer.android.com/training/basics/fragments/communicating.html)
-     * for more information.
-     */
     interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
-        fun onFragmentInteraction(uri: Uri)
+        fun onFragmentInteraction(page: Int)
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DynamicFormFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-                DynamicFormFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
+        fun newInstance() = DynamicFormFragment()
     }
 }
