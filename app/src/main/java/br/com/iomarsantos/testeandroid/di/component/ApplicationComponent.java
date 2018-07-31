a64d9e52c6ea33322aa2a59e613e606b37fbf8d3
Iomar Santos <iomar.santos@outlook.com>
2018-07-31 14:50:47 -0300

    Implementado Component global para realizar a comunicacao entre module e provides

---
+++ app/src/main/java/br/com/iomarsantos/testeandroid/di/component/ApplicationComponent.java
@@ -0,0 +1,24 @@
+package br.com.iomarsantos.testeandroid.di.component;
+
+import android.app.Application;
+import android.content.Context;
+
+import javax.inject.Singleton;
+
+import br.com.iomarsantos.testeandroid.TesteAndroidApp;
+import br.com.iomarsantos.testeandroid.di.ApplicationContext;
+import br.com.iomarsantos.testeandroid.di.module.ApplicationModule;
+import dagger.Component;
+
+@Singleton
+@Component(modules = ApplicationModule.class)
+public interface ApplicationComponent {
+
+    void inject(TesteAndroidApp testeAndroidApp);
+
+    @ApplicationContext
+    Context context();
+
+    Application application();
+
+}
