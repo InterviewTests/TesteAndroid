91ea336dc02b5db4e640d7343eda14a879053b59
Iomar Santos <iomar.santos@outlook.com>
2018-07-31 17:48:06 -0300

    Criada interface que sera implementada em cada presenter e injetada em actvity ou fragment

---
+++ app/src/main/java/br/com/iomarsantos/testeandroid/ui/base/Presenter.java
@@ -0,0 +1,10 @@
+package br.com.iomarsantos.testeandroid.ui.base;
+
+/**
+ * Cada presenter deve implementar essa interface ou estender o BasePresenter
+ * indicando o tipo BaseView que deseja ser anexado.
+ */
+public interface Presenter<V extends BaseView> {
+
+}
+
