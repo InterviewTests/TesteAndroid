ad7cd3b4f7474c623c4d36f776ace713c95285bf
Iomar Santos <iomar.santos@outlook.com>
2018-07-31 12:08:05 -0300

    Criada a classe base TesteAndroidApp para manter o estado global do aplicativo

---
+++ app/src/main/java/br/com/iomarsantos/testeandroid/TesteAndroidApp.java
@@ -0,0 +1,11 @@
+package br.com.iomarsantos.testeandroid;
+
+import android.app.Application;
+
+public class TesteAndroidApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
