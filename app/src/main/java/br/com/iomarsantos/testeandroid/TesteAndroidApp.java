1e0c9ff46f2af7c7fc6e418a422af0c503d5bf82
Iomar Santos <iomar.santos@outlook.com>
2018-07-31 15:19:02 -0300

    adicionado injeção de dependência

--- app/src/main/java/br/com/iomarsantos/testeandroid/TesteAndroidApp.java
+++ app/src/main/java/br/com/iomarsantos/testeandroid/TesteAndroidApp.java
@@ -2,10 +2,31 @@ package br.com.iomarsantos.testeandroid;
 
 import android.app.Application;
 
+import br.com.iomarsantos.testeandroid.di.component.ApplicationComponent;
+import br.com.iomarsantos.testeandroid.di.component.DaggerApplicationComponent;
+import br.com.iomarsantos.testeandroid.di.module.ApplicationModule;
+
 public class TesteAndroidApp extends Application {
+
+    private ApplicationComponent mApplicationComponent;
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        mApplicationComponent = DaggerApplicationComponent.builder()
+                .applicationModule(new ApplicationModule(this)).build();
+
+        mApplicationComponent.inject(this);
+
     }
+
+    public ApplicationComponent getComponent() {
+        return mApplicationComponent;
+    }
+
+    public void setComponent(ApplicationComponent applicationComponent) {
+        mApplicationComponent = applicationComponent;
+    }
+
 }
