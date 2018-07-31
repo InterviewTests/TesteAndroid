4a8c58f68bc811ccd7aa9b070451b0c5794a58ab
Iomar Santos <iomar.santos@outlook.com>
2018-07-31 19:54:52 -0300

    adicionado metodo para possibilitar a injeção de dependência

--- app/src/main/java/br/com/iomarsantos/testeandroid/di/component/ActivityComponent.java
+++ app/src/main/java/br/com/iomarsantos/testeandroid/di/component/ActivityComponent.java
@@ -2,10 +2,11 @@ package br.com.iomarsantos.testeandroid.di.component;
 
 import br.com.iomarsantos.testeandroid.di.PerActivity;
 import br.com.iomarsantos.testeandroid.di.module.ActivityModule;
+import br.com.iomarsantos.testeandroid.ui.splash.SplashActivity;
 import dagger.Component;
 
 @PerActivity
 @Component(dependencies = ApplicationComponent.class, modules = ActivityModule.class)
 public interface ActivityComponent {
-
+    void inject(SplashActivity splashActivity);
 }
