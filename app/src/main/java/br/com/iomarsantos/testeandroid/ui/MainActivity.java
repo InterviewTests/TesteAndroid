2800f4ff569f71014c3bdb9bbf5988023c9914e7
Iomar Santos <iomar.santos@outlook.com>
2018-07-31 11:59:46 -0300

    Criado pacote ui e movido a classe MainActivity para o mesmo

--- app/src/main/java/br/com/iomarsantos/testeandroid/ui/MainActivity.java
+++ app/src/main/java/br/com/iomarsantos/testeandroid/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package br.com.iomarsantos.testeandroid;
+package br.com.iomarsantos.testeandroid.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import br.com.iomarsantos.testeandroid.R;
 
 public class MainActivity extends AppCompatActivity {
 
