555a4ad0ed88e229e8239faee7e175336969e7d3
Iomar Santos <iomar.santos@outlook.com>
2018-07-31 19:31:17 -0300

    Implementado os metodos onAttach e onDetach para anexar ou desvincular uma Activity ou Fragment

--- app/src/main/java/br/com/iomarsantos/testeandroid/ui/base/BasePresenter.java
+++ app/src/main/java/br/com/iomarsantos/testeandroid/ui/base/BasePresenter.java
@@ -2,6 +2,8 @@ package br.com.iomarsantos.testeandroid.ui.base;
 
 import javax.inject.Inject;
 
+import io.reactivex.disposables.CompositeDisposable;
+
 /**
  * Classe base que implementa a interface Presenter e fornece uma implementação básica para adicionar ou remover uma referencia a uma view
  */
@@ -9,17 +11,37 @@ public class BasePresenter<V extends BaseView> implements Presenter<V> {
 
     private static final String TAG = "BasePresenter";
 
+    private final CompositeDisposable mCompositeDisposable;
+
+    private V mBaseView;
+
     @Inject
-    public BasePresenter() {}
+    public BasePresenter(CompositeDisposable mCompositeDisposable) {
+        this.mCompositeDisposable = mCompositeDisposable;
+    }
 
     @Override
     public void onAttach(V baseView) {
-
+        this.mBaseView = baseView;
     }
 
     @Override
     public void onDetach() {
-
+        this.mCompositeDisposable.dispose();
+        this.mBaseView = null;
     }
+
+    public V getView() {
+        return this.mBaseView;
+    }
+
+    public boolean isViewAttached() {
+        return this.mBaseView != null;
+    }
+
+    public CompositeDisposable getCompositeDisposable() {
+        return mCompositeDisposable;
+    }
+
 
 }
