cf657d807decdfcd5f4800cfee9e07c410df6d0b
Iomar Santos <iomar.santos@outlook.com>
2018-07-31 16:00:51 -0300

    Criada interface base que representa uma view no MVP (Model View Presenter),interface que possibilita a comunicacao entre presenter e view(Activity ou Fragment)

---
+++ app/src/main/java/br/com/iomarsantos/testeandroid/ui/base/BaseView.java
@@ -0,0 +1,9 @@
+package br.com.iomarsantos.testeandroid.ui.base;
+
+/**
+ * Interface base que ser estendida por outra interface mais especifica que sera implementada por uma Activity ou Fragment
+ * todas as classes que vao atuar como View no padrão MVP, devem implementar uma interface especifica.
+ */
+public interface BaseView {
+
+}
