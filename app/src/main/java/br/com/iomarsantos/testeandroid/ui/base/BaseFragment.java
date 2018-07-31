7c669d7f51ff795b16c4c281c4dbbddbb1c3719b
Iomar Santos <iomar.santos@outlook.com>
2018-07-31 17:27:39 -0300

    Implementado BaseFragment que fornece acesso a classe ActivityComponent

---
+++ app/src/main/java/br/com/iomarsantos/testeandroid/ui/base/BaseFragment.java
@@ -0,0 +1,46 @@
+package br.com.iomarsantos.testeandroid.ui.base;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.View;
+
+import br.com.iomarsantos.testeandroid.di.component.ActivityComponent;
+
+public abstract class BaseFragment extends Fragment implements BaseView {
+
+    private BaseActivity mActivity;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof BaseActivity) {
+            BaseActivity activity = (BaseActivity) context;
+            this.mActivity = activity;
+        }
+    }
+
+    public ActivityComponent getActivityComponent() {
+        if (mActivity != null) {
+            return mActivity.getActivityComponent();
+        }
+        return null;
+    }
+
+    public BaseActivity getBaseActivity() {
+        return mActivity;
+    }
+
+}
