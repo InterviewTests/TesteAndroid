d8cecb165fe716bd81996dd05cd44d80bdf9f5ef
Iomar Santos <iomar.santos@outlook.com>
2018-07-31 13:57:37 -0300

    Criado tipos de anotacoes

---
+++ app/src/main/java/br/com/iomarsantos/testeandroid/di/ApplicationContext.java
@@ -0,0 +1,11 @@
+package br.com.iomarsantos.testeandroid.di;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Qualifier;
+
+@Qualifier
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ApplicationContext {
+}
---
+++ app/src/main/java/br/com/iomarsantos/testeandroid/di/PerActivity.java
@@ -0,0 +1,11 @@
+package br.com.iomarsantos.testeandroid.di;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Scope;
+
+@Scope
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PerActivity {
+}
