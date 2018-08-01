30a76aa1933a66ae4974a3098d0dd38882161e33
Iomar Santos <iomar.santos@outlook.com>
2018-08-01 15:17:20 -0300

    Configurado butterknife para obter referencias das views

--- app/src/main/java/br/com/iomarsantos/testeandroid/ui/base/BaseActivity.java
+++ app/src/main/java/br/com/iomarsantos/testeandroid/ui/base/BaseActivity.java
@@ -8,10 +8,13 @@ import br.com.iomarsantos.testeandroid.TesteAndroidApp;
 import br.com.iomarsantos.testeandroid.di.component.ActivityComponent;
 import br.com.iomarsantos.testeandroid.di.component.DaggerActivityComponent;
 import br.com.iomarsantos.testeandroid.di.module.ActivityModule;
+import butterknife.Unbinder;
 
 public abstract class BaseActivity extends AppCompatActivity implements BaseView {
 
     private ActivityComponent mActivityComponent;
+
+    private Unbinder mUnBinder;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,6 +27,18 @@ public abstract class BaseActivity extends AppCompatActivity implements BaseView
 
     public ActivityComponent getActivityComponent() {
         return mActivityComponent;
+    }
+
+    public void setUnBinder(Unbinder unBinder) {
+        mUnBinder = unBinder;
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mUnBinder != null) {
+            mUnBinder.unbind();
+        }
+        super.onDestroy();
     }
 
     protected abstract void setup();
