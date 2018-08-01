08aca32a39ede11945c4861a7b122d0611fcbc37
Iomar Santos <iomar.santos@outlook.com>
2018-08-01 14:35:37 -0300

    adicionado metodos para possibilitar a injeção de dependência

--- app/src/main/java/br/com/iomarsantos/testeandroid/di/component/ActivityComponent.java
+++ app/src/main/java/br/com/iomarsantos/testeandroid/di/component/ActivityComponent.java
@@ -3,6 +3,8 @@ package br.com.iomarsantos.testeandroid.di.component;
 import br.com.iomarsantos.testeandroid.di.PerActivity;
 import br.com.iomarsantos.testeandroid.di.module.ActivityModule;
 import br.com.iomarsantos.testeandroid.ui.fundo.FundoActivity;
+import br.com.iomarsantos.testeandroid.ui.fundo.contato.ContatoFragment;
+import br.com.iomarsantos.testeandroid.ui.fundo.investimentos.InvestimentoFragment;
 import br.com.iomarsantos.testeandroid.ui.splash.SplashActivity;
 import dagger.Component;
 
@@ -11,4 +13,6 @@ import dagger.Component;
 public interface ActivityComponent {
     void inject(SplashActivity splashActivity);
     void inject(FundoActivity fundoActivity);
+    void inject(InvestimentoFragment investimentoFragment);
+    void inject(ContatoFragment contatoFragment);
 }
