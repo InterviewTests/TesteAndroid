d7fc8d69f60b83d65b1a98a74e0af3edf5503268
Iomar Santos <iomar.santos@outlook.com>
2018-08-01 23:35:25 -0300

    Adicionado novos metodos

--- app/src/main/java/br/com/iomarsantos/testeandroid/ui/base/BaseView.java
+++ app/src/main/java/br/com/iomarsantos/testeandroid/ui/base/BaseView.java
@@ -1,9 +1,13 @@
 package br.com.iomarsantos.testeandroid.ui.base;
 
+import android.support.annotation.StringRes;
+
 /**
  * Interface base que ser estendida por outra interface mais especifica que sera implementada por uma Activity ou Fragment
  * todas as classes que vao atuar como View no padrão MVP, devem implementar uma interface especifica.
  */
 public interface BaseView {
-
+    void setup();
+    void setTitleActivity(@StringRes int resId);
+    void setTitleActivity(String titulo);
 }
