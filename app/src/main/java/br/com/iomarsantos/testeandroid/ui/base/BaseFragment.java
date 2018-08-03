52df66e934b81f92e5bb8961ca248d432cc6034f
Iomar Santos <iomar.santos@outlook.com>
2018-08-03 16:57:38 -0300

    Implementado metodos para exibir mensagem e configurar views

--- app/src/main/java/br/com/iomarsantos/testeandroid/ui/base/BaseFragment.java
+++ app/src/main/java/br/com/iomarsantos/testeandroid/ui/base/BaseFragment.java
@@ -1,5 +1,6 @@
 package br.com.iomarsantos.testeandroid.ui.base;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -8,10 +9,16 @@ import android.support.v4.app.Fragment;
 import android.view.View;
 
 import br.com.iomarsantos.testeandroid.di.component.ActivityComponent;
+import br.com.iomarsantos.testeandroid.utils.DialogLoadingUtils;
+import butterknife.Unbinder;
 
 public abstract class BaseFragment extends Fragment implements BaseView {
 
     private BaseActivity mActivity;
+
+    private Unbinder mUnBinder;
+
+    private ProgressDialog mProgressDialog;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -22,6 +29,7 @@ public abstract class BaseFragment extends Fragment implements BaseView {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        setup();
     }
 
     @Override
@@ -31,6 +39,54 @@ public abstract class BaseFragment extends Fragment implements BaseView {
             BaseActivity activity = (BaseActivity) context;
             this.mActivity = activity;
         }
+    }
+
+    @Override
+    public void showLoading() {
+        hideLoading();
+        mProgressDialog = DialogLoadingUtils.showLoadingDialog(this.getContext());
+    }
+
+    @Override
+    public void hideLoading() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.cancel();
+        }
+    }
+
+    @Override
+    public void onError(String message) {
+        if (mActivity != null) {
+            mActivity.onError(message);
+        }
+    }
+
+    @Override
+    public void onError(@StringRes int resId) {
+        if (mActivity != null) {
+            mActivity.onError(resId);
+        }
+    }
+
+    @Override
+    public void showMessage(String message) {
+        if (mActivity != null) {
+            mActivity.showMessage(message);
+        }
+    }
+
+    @Override
+    public void showMessage(@StringRes int resId) {
+        if (mActivity != null) {
+            mActivity.showMessage(resId);
+        }
+    }
+
+
+    @Override
+    public void onDetach() {
+        mActivity = null;
+        super.onDetach();
     }
 
     public ActivityComponent getActivityComponent() {
@@ -43,6 +99,19 @@ public abstract class BaseFragment extends Fragment implements BaseView {
     public BaseActivity getBaseActivity() {
         return mActivity;
     }
+
+    public void setUnBinder(Unbinder unBinder) {
+        mUnBinder = unBinder;
+    }
+
+    @Override
+    public void onDestroy() {
+        if (mUnBinder != null) {
+            mUnBinder.unbind();
+        }
+        super.onDestroy();
+    }
+
 
     @Override
     public void setup() {
