193ad5c529f7f96dc6fca47ea08df13c66f2cd5d
Iomar Santos <iomar.santos@outlook.com>
2018-08-03 16:43:57 -0300

    Adicionado metodo para chamar api

--- app/src/main/java/br/com/iomarsantos/testeandroid/ui/fundo/contato/ContatoBasePresenter.java
+++ app/src/main/java/br/com/iomarsantos/testeandroid/ui/fundo/contato/ContatoBasePresenter.java
@@ -1,8 +1,7 @@
 package br.com.iomarsantos.testeandroid.ui.fundo.contato;
 
-import br.com.iomarsantos.testeandroid.di.PerActivity;
 import br.com.iomarsantos.testeandroid.ui.base.Presenter;
 
 public interface ContatoBasePresenter<V extends ContatoView> extends Presenter<V> {
-
+    void findAllCellsApiCall();
 }
