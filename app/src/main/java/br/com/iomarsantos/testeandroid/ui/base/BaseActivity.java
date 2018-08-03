a27f9b88d3a0d39df4a82f72995494ddc2ee2684
Iomar Santos <iomar.santos@outlook.com>
2018-08-03 16:56:46 -0300

    Implementado metodos para exibir mensagens

--- app/src/main/java/br/com/iomarsantos/testeandroid/ui/base/BaseActivity.java
+++ app/src/main/java/br/com/iomarsantos/testeandroid/ui/base/BaseActivity.java
@@ -1,14 +1,22 @@
 package br.com.iomarsantos.testeandroid.ui.base;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import br.com.iomarsantos.testeandroid.R;
 import br.com.iomarsantos.testeandroid.TesteAndroidApp;
 import br.com.iomarsantos.testeandroid.di.component.ActivityComponent;
 import br.com.iomarsantos.testeandroid.di.component.DaggerActivityComponent;
 import br.com.iomarsantos.testeandroid.di.module.ActivityModule;
+import br.com.iomarsantos.testeandroid.utils.DialogLoadingUtils;
 import butterknife.Unbinder;
 
 public abstract class BaseActivity extends AppCompatActivity implements BaseView {
@@ -16,6 +24,8 @@ public abstract class BaseActivity extends AppCompatActivity implements BaseView
     private ActivityComponent mActivityComponent;
 
     private Unbinder mUnBinder;
+
+    private ProgressDialog mProgressDialog;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -50,5 +60,57 @@ public abstract class BaseActivity extends AppCompatActivity implements BaseView
 
     @Override
     public void setTitleActivity(String titulo) {}
+
+
+    @Override
+    public void showLoading() {
+        hideLoading();
+        mProgressDialog = DialogLoadingUtils.showLoadingDialog(this);
+    }
+
+    @Override
+    public void hideLoading() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.cancel();
+        }
+    }
+
+    private void showSnackBar(String message) {
+        Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content),
+                message, Snackbar.LENGTH_SHORT);
+        View sbView = snackbar.getView();
+        TextView textView = (TextView) sbView
+                .findViewById(android.support.design.R.id.snackbar_text);
+        textView.setTextColor(ContextCompat.getColor(this, R.color.white));
+        snackbar.show();
+    }
+
+    @Override
+    public void onError(String message) {
+        if (message != null) {
+            showSnackBar(message);
+        } else {
+            showSnackBar(getString(R.string.msg_error));
+        }
+    }
+
+    @Override
+    public void onError(@StringRes int resId) {
+        onError(getString(resId));
+    }
+
+    @Override
+    public void showMessage(String message) {
+        if (message != null) {
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, getString(R.string.msg_error), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void showMessage(@StringRes int resId) {
+        showMessage(getString(resId));
+    }
 
 }
