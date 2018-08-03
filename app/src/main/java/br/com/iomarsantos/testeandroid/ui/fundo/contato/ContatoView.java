245f42a12da1fccd05cd221535f69c529764fc54
Iomar Santos <iomar.santos@outlook.com>
2018-08-03 16:39:10 -0300

    Adicionado novos metodos

--- app/src/main/java/br/com/iomarsantos/testeandroid/ui/fundo/contato/ContatoView.java
+++ app/src/main/java/br/com/iomarsantos/testeandroid/ui/fundo/contato/ContatoView.java
@@ -1,7 +1,15 @@
 package br.com.iomarsantos.testeandroid.ui.fundo.contato;
 
+import android.view.View;
+import android.widget.CheckBox;
+
 import br.com.iomarsantos.testeandroid.ui.base.BaseView;
 
 public interface ContatoView extends BaseView {
-
+    void addView(View view);
+    void configureEmailField(View view);
+    void configuraPhoneField(View view);
+    void configureTextField(View view);
+    void visibilityForCellTypeFieldEmailView(int visibility, CheckBox checkBox);
+    void send();
 }
