d0919281b94c70492eec6f29fe6246eea7b7b0c0
Iomar Santos <iomar.santos@outlook.com>
2018-08-03 16:47:12 -0300

    Adicionado metodo para tratar erro api

--- app/src/main/java/br/com/iomarsantos/testeandroid/ui/base/Presenter.java
+++ app/src/main/java/br/com/iomarsantos/testeandroid/ui/base/Presenter.java
@@ -1,5 +1,7 @@
 package br.com.iomarsantos.testeandroid.ui.base;
 
+import com.androidnetworking.error.ANError;
+
 /**
  * Cada presenter deve implementar essa interface ou estender o BasePresenter
  * indicando o tipo BaseView que deseja ser anexado.
@@ -7,5 +9,6 @@ package br.com.iomarsantos.testeandroid.ui.base;
 public interface Presenter<V extends BaseView> {
     void onAttach(V baseView);
     void onDetach();
+    void handleApiError(ANError error);
 }
 
