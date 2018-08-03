8b5da009a6fb9653e036fb3625f33bb27083672e
Iomar Santos <iomar.santos@outlook.com>
2018-08-03 16:34:18 -0300

    Adicionado arquivo endpoint

---
+++ app/src/main/java/br/com/iomarsantos/testeandroid/data/network/EndPoint.java
@@ -0,0 +1,13 @@
+package br.com.iomarsantos.testeandroid.data.network;
+
+import br.com.iomarsantos.testeandroid.BuildConfig;
+
+public final class EndPoint {
+
+    public static final String ENDPOINT_CELLS = BuildConfig.BASE_URL + "/cells.json";
+
+    private EndPoint() {
+
+    }
+
+}
