9cbdf39f5a1b40af8b00599a7e898b0696a71757
Iomar Santos <iomar.santos@outlook.com>
2018-08-03 16:36:52 -0300

    Adicionado arquivo de repositorio

---
+++ app/src/main/java/br/com/iomarsantos/testeandroid/data/AppRepository.java
@@ -0,0 +1,33 @@
+package br.com.iomarsantos.testeandroid.data;
+
+import android.content.Context;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import br.com.iomarsantos.testeandroid.data.model.CellResponse;
+import br.com.iomarsantos.testeandroid.data.network.Api;
+import br.com.iomarsantos.testeandroid.di.ApplicationContext;
+import io.reactivex.Single;
+
+@Singleton
+public class AppRepository implements Repository {
+
+    private static final String TAG = "AppRepository";
+
+    private final Context mContext;
+    private final Api mApi;
+
+    @Inject
+    public AppRepository(@ApplicationContext Context context,
+                         Api api) {
+        mContext = context;
+        mApi = api;
+    }
+
+    @Override
+    public Single<CellResponse> getCellApiCall() {
+        return this.mApi.getCellApiCall();
+    }
+
+}
---
+++ app/src/main/java/br/com/iomarsantos/testeandroid/data/Repository.java
@@ -0,0 +1,7 @@
+package br.com.iomarsantos.testeandroid.data;
+
+import br.com.iomarsantos.testeandroid.data.network.Api;
+
+public interface Repository extends Api {
+
+}
