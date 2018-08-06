21040031a278b8e6df170c0dfc561cc84e074bec
Iomar Santos <iomar.santos@outlook.com>
2018-08-06 10:31:19 -0300

    Adicionado configuracao da biblioteca de rede e parse de dados, injetado classe para permitir usar fontes externas

--- app/src/main/java/br/com/iomarsantos/testeandroid/TesteAndroidApp.java
+++ app/src/main/java/br/com/iomarsantos/testeandroid/TesteAndroidApp.java
@@ -2,13 +2,23 @@ package br.com.iomarsantos.testeandroid;
 
 import android.app.Application;
 
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.interceptors.HttpLoggingInterceptor;
+import com.jacksonandroidnetworking.JacksonParserFactory;
+
+import javax.inject.Inject;
+
 import br.com.iomarsantos.testeandroid.di.component.ApplicationComponent;
 import br.com.iomarsantos.testeandroid.di.component.DaggerApplicationComponent;
 import br.com.iomarsantos.testeandroid.di.module.ApplicationModule;
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 
 public class TesteAndroidApp extends Application {
 
     private ApplicationComponent mApplicationComponent;
+
+    @Inject
+    CalligraphyConfig mCalligraphyConfig;
 
     @Override
     public void onCreate() {
@@ -18,6 +28,16 @@ public class TesteAndroidApp extends Application {
                 .applicationModule(new ApplicationModule(this)).build();
 
         mApplicationComponent.inject(this);
+
+        AndroidNetworking.initialize(getApplicationContext());
+
+        AndroidNetworking.setParserFactory(new JacksonParserFactory());
+
+        if (BuildConfig.DEBUG) {
+            AndroidNetworking.enableLogging(HttpLoggingInterceptor.Level.BODY);
+        }
+
+        CalligraphyConfig.initDefault(mCalligraphyConfig);
 
     }
 
