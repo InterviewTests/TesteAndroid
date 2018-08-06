e1427ff280c07ba52f3341e1144dc763281c0e2e
Iomar Santos <iomar.santos@outlook.com>
2018-08-06 11:20:25 -0300

    Adicionado metodo para permitir chamar api

--- app/src/main/java/br/com/iomarsantos/testeandroid/ui/fundo/investimentos/InvestimentoBasePresenter.java
+++ app/src/main/java/br/com/iomarsantos/testeandroid/ui/fundo/investimentos/InvestimentoBasePresenter.java
@@ -1,8 +1,7 @@
 package br.com.iomarsantos.testeandroid.ui.fundo.investimentos;
 
-import br.com.iomarsantos.testeandroid.di.PerActivity;
 import br.com.iomarsantos.testeandroid.ui.base.Presenter;
 
 public interface InvestimentoBasePresenter<V extends InvestimentoView> extends Presenter<V> {
-
+    void findAllFundApiCall();
 }
