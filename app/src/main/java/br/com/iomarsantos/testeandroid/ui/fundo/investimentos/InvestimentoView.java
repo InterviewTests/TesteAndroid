abe304c0a2f660defff6437f83d2bdf8d523c9f6
Iomar Santos <iomar.santos@outlook.com>
2018-08-06 10:58:57 -0300

    Adicionado metodo para permitir atualizar a tela

--- app/src/main/java/br/com/iomarsantos/testeandroid/ui/fundo/investimentos/InvestimentoView.java
+++ app/src/main/java/br/com/iomarsantos/testeandroid/ui/fundo/investimentos/InvestimentoView.java
@@ -1,7 +1,8 @@
 package br.com.iomarsantos.testeandroid.ui.fundo.investimentos;
 
+import br.com.iomarsantos.testeandroid.entity.InvestmentFund;
 import br.com.iomarsantos.testeandroid.ui.base.BaseView;
 
 public interface InvestimentoView extends BaseView {
-
+    void updateVies(InvestmentFund investmentFund);
 }
