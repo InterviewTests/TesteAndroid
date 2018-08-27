95bd239912dd07dbde3c9c0086218c87b4e32f3c
David Canon <david.canon@resourceit.com>
2018-08-27 14:20:32 -0300

    Interface contendo o contrato das chamadas de API

---
+++ app/src/main/java/br/com/santander/testeandroid/api/Api.java
@@ -0,0 +1,27 @@
+package br.com.santander.testeandroid.api;
+
+import br.com.santander.testeandroid.contact.model.GetCellsResponse;
+import br.com.santander.testeandroid.investment.model.InvestmentResponse;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+/**
+ * Interface to map API calls contract.
+ */
+public interface Api {
+
+    String BASE_URL = "https://floating-mountain-50292.herokuapp.com/";
+
+    /**
+     * Retrieve investment funds data
+     */
+    @GET("/fund.json")
+    Call<InvestmentResponse> getFound();
+
+    /**
+     * Retrieve cells data
+     */
+    @GET("/cells.json")
+    Call<GetCellsResponse> getCells();
+
+}
