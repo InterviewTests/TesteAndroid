c205abd047d05c1b033251025e50067b3ef5ae40
Alex Castanha <adcdeveloper@users.noreply.github.com>
2018-08-28 13:50:37 -0300

    Changed: refac TypeField with StringDef / Added: icon BottomNavigationView

--- app/src/main/java/com/alex/testeandroid/data/entities/contact/TypeField.java
+++ app/src/main/java/com/alex/testeandroid/data/entities/contact/TypeField.java
@@ -1,6 +1,7 @@
 package com.alex.testeandroid.data.entities.contact;
 
 import android.support.annotation.IntDef;
+import android.support.annotation.StringDef;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -12,6 +13,7 @@ public class TypeField {
 
     //region CONSTANTS
     public static final int TEXT = 1, TEL_NUMBER = 2, EMAIL = 3;
+    public static final String TEXT_DESC = "text", TEL_NUMBER_DESC = "telnumber", EMAIL_DESC = "email";
     //endregion
 
     //region FIELDS
@@ -19,11 +21,11 @@ public class TypeField {
     //endregion
 
     //region CONSTRUCT
-    public TypeField(int id) {
+    public TypeField(@TypeFieldCell int id) {
         this.id = id;
     }
 
-    public TypeField(String desc) {
+    public TypeField(@TypeFieldDescCell String desc) {
         if (desc.equals("text")) {
             this.id = TEXT;
         } else if (desc.equals("telnumber")) {
@@ -44,6 +46,11 @@ public class TypeField {
     @IntDef({TEXT, TEL_NUMBER, EMAIL})
     @Retention(RetentionPolicy.SOURCE)
     public @interface TypeFieldCell {
+    }
+
+    @StringDef({TEXT_DESC, TEL_NUMBER_DESC, EMAIL_DESC})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface TypeFieldDescCell {
     }
     //endregion
 }
