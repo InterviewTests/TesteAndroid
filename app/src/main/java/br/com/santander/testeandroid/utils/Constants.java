81fb2cb4df6d769d31ea90778ff54849a0112c68
David Canon <david.canon@resourceit.com>
2018-08-28 15:40:11 -0300

    Atualizando resources do projeto

--- app/src/main/java/br/com/santander/testeandroid/utils/Constants.java
+++ app/src/main/java/br/com/santander/testeandroid/utils/Constants.java
@@ -2,5 +2,15 @@ package br.com.santander.testeandroid.utils;
 
 public class Constants {
     public static final int NETWORK_TIMEOUT = 20000;
-    public static final int WRITE_TIMEOUT   = 30000;
+    public static final int WRITE_TIMEOUT = 30000;
+    public static final String PHONE_TYPE_FIELD = "telnumber";
+    public final static int TYPE_FIELD = 1;
+    public final static int TYPE_TEXT = 2;
+    public final static int TYPE_IMAGE = 3;
+    public final static int TYPE_CHECKBOX = 4;
+    public final static int TYPE_SEND = 5;
+    public final static String INPUT_TYPE_TEXT = "1";
+    public final static String INPUT_TYPE_TEL_NUMBER = "telnumber";
+    public final static String INPUT_TYPE_EMAIL = "3";
+    public final static String INVALID_FIELD = "Campo inválido";
 }
