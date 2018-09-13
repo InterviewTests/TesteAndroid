a0991656918c12b2bed78d209522d0013b47064c
khalilayache <uJ4iTArnd2ImF9JUyBal>
2018-09-13 14:41:39 -0300

    create Investment fragment layout

--- app/src/main/java/santander/com/br/invest/ui/MainActivity.kt
+++ app/src/main/java/santander/com/br/invest/ui/MainActivity.kt
@@ -1,8 +1,29 @@
 package santander.com.br.invest.ui
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import santander.com.br.invest.R
+import santander.com.br.invest.ui.adapter.ViewPagerAdapter
+import santander.com.br.invest.ui.fragments.InvestmentFragment
 
 
 class MainActivity : AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initViewPager()
+    }
+
+    private fun initViewPager() {
+        viewPager.offscreenPageLimit = 2
+        tabLayout.setupWithViewPager(viewPager)
+
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+        adapter.addFragment(InvestmentFragment(), "Investimento")
+        adapter.addFragment(InvestmentFragment(), "Contato")
+        viewPager.adapter = adapter
+    }
 }
