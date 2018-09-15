60232d8acdb490755ad9da0974a9d651841185c4
Luiz Lago <luiz.lago@4move.me>
2018-09-15 16:18:41 -0300

    Adicionando a ViewPager e o TabLayout Configurando a também a cor da statusbar

--- app/src/main/java/com/santander/luizlago/testeandroid/MainActivity.kt
+++ app/src/main/java/com/santander/luizlago/testeandroid/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.santander.luizlago.testeandroid
 
+import android.graphics.Color
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        configureStatusBar()
+    }
+
+    private fun configureStatusBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val window = window
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            window.statusBarColor = Color.TRANSPARENT
+        }
     }
 }
