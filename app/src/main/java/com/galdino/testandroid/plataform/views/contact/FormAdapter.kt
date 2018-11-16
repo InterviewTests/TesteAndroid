861a6a04891ab0b6e604a161b23873e2b674bb1d
GaldinoMadeInWeb <galdino.ouros@madeinweb.com.br>
2018-11-16 18:18:22 -0200

    Estruturação da requisição da tela de contatos com o clean architecture finalizada e testada

--- app/src/main/java/com/galdino/testandroid/plataform/views/contact/FormAdapter.kt
+++ app/src/main/java/com/galdino/testandroid/plataform/views/contact/FormAdapter.kt
@@ -7,9 +7,11 @@ import android.view.ViewGroup
 import com.galdino.testandroid.R
 import com.galdino.testandroid.domain.model.Cell
 import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.adapter_form_checkbox.*
+import kotlinx.android.synthetic.main.adapter_form_edit_text.*
 import kotlinx.android.synthetic.main.adapter_form_text_view.*
 
-class FormAdapter(val mList: List<Cell>): RecyclerView.Adapter<FormAdapter.ViewHolder>() {
+class FormAdapter(private val mList: List<Cell>): RecyclerView.Adapter<FormAdapter.ViewHolder>() {
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
         val inflater= LayoutInflater.from(viewGroup.context)
         val view = when(viewType) {
@@ -38,8 +40,14 @@ class FormAdapter(val mList: List<Cell>): RecyclerView.Adapter<FormAdapter.ViewH
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
         when(getItemViewType(position))
         {
+            Cell.Type.FIELD->{
+                viewHolder.tilCell.hint = mList[position].message
+            }
             Cell.Type.TEXT->{
                 viewHolder.tvCell.text = mList[position].message
+            }
+            Cell.Type.CHECK_BOX->{
+                viewHolder.cbCell.text = mList[position].message
             }
         }
     }
