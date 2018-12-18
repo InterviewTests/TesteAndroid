6d01ee7b07a641c43daab06651b2a34603e3eea7
Paulo Age <paulo.age@outlook.com>
2018-12-18 21:04:48 -0200

    Configuracao das dependecias e packages iniciais

---
+++ app/src/main/java/com/seletiva/santander/investment/UI/Activities/BaseActivity.java
@@ -0,0 +1,8 @@
+package com.seletiva.santander.investment.UI.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+
+}
+
---
+++ app/src/main/java/com/seletiva/santander/investment/UI/Activities/MainActivity.java
@@ -0,0 +1,10 @@
+package com.seletiva.santander.investment.UI.Activities;
+
+import com.seletiva.santander.investment.R;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends BaseActivity {
+
+}
