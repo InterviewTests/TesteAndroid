c06f0303573e2ac7626b8d5ef577fe1279b70d2f
rafael <rafael.felipe1989@gmail.com>
2019-01-12 21:05:01 -0200

    Investimento Presnter

---
+++ SantanderTeste/app/src/main/java/br/com/rafael/santanderteste/presentation/FunPresenter.java
@@ -0,0 +1,34 @@
+package br.com.rafael.santanderteste.presentation;
+
+import br.com.rafael.santanderteste.domain.ScreenFund;
+import br.com.rafael.santanderteste.repository.BankProvider;
+import br.com.rafael.santanderteste.repository.BankRepository;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class FunPresenter implements FundContract.Presenter {
+
+    private FundContract.View view;
+
+
+    @Override
+    public void setView(@NotNull FundContract.View view) {
+        this.view = view;
+    }
+
+    @Override
+    public void retrieveInvestimentData() {
+        view.loadingInvestimentData();
+        BankProvider.Companion.getFund(new BankRepository<ScreenFund>() {
+            @Override
+            public void onResponse(@Nullable ScreenFund reponse) {
+                view.showInvestimentData(reponse);
+            }
+
+            @Override
+            public void onFailure(@NotNull Throwable throwable) {
+                view.showErrorToRetrieveData();
+            }
+        });
+    }
+}
