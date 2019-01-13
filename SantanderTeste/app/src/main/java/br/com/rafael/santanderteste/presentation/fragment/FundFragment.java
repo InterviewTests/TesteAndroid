377d161f42891b1eec9e4ae4c59a00cdbb52484a
rafael <rafael.felipe1989@gmail.com>
2019-01-13 00:12:08 -0200

    removido import sem uso

--- SantanderTeste/app/src/main/java/br/com/rafael/santanderteste/presentation/fragment/FundFragment.java
+++ SantanderTeste/app/src/main/java/br/com/rafael/santanderteste/presentation/fragment/FundFragment.java
@@ -6,14 +6,11 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 import br.com.rafael.santanderteste.R;
 import br.com.rafael.santanderteste.domain.Fund;
 import br.com.rafael.santanderteste.domain.GeneralInfo;
@@ -24,7 +21,6 @@ import br.com.rafael.santanderteste.presentation.FunPresenter;
 import br.com.rafael.santanderteste.presentation.FundContract;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
