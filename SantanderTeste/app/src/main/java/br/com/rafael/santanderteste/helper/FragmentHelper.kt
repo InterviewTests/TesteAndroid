312a7b26421f8058f7c87e97f582d9cc150009f4
rafael <rafael.felipe1989@gmail.com>
2019-01-12 22:59:31 -0200

    Logica para verificar se cada um dos Fragments de Investimentos e Formulario devem ser recriado ou apenas exibidos

--- SantanderTeste/app/src/main/java/br/com/rafael/santanderteste/helper/FragmentHelper.kt
+++ SantanderTeste/app/src/main/java/br/com/rafael/santanderteste/helper/FragmentHelper.kt
@@ -2,20 +2,39 @@ package br.com.rafael.santanderteste.helper
 
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
+import br.com.rafael.santanderteste.presentation.fragment.FundFragment
 
 class FragmentHelper {
 
     companion object {
         /**
          * Atualiza um Fragment que ja esta adicionado em um layout view por outro
+         * Verifica se o fragment esta nulo para criar ou apenas exibir
          * @param supportFragmentManager FragmentManager
          * @param layoutReference O id do layout que servira de container para o Fragment
          * @param fragment Fragment que sera adicionado/atualizado no container de layout
          */
-        fun replace_framgment(supportFragmentManager: FragmentManager, layoutReference: Int, fragment: Fragment) {
-            var fragmentTransaction = supportFragmentManager.beginTransaction()
-            fragmentTransaction.replace(layoutReference, fragment)
-            fragmentTransaction.commit()
+        fun replace_framgment(supportFragmentManager: FragmentManager, layoutReference: Int, fragment: Fragment, tag : String) {
+            if(tag.equals("invest")) {
+                if(supportFragmentManager.findFragmentByTag("invest") != null) {
+                    supportFragmentManager.beginTransaction().show(supportFragmentManager.findFragmentByTag("invest")).commit();
+                } else {
+                    supportFragmentManager.beginTransaction().add(layoutReference, fragment, "invest").commit();
+                }
+                if(supportFragmentManager.findFragmentByTag("contact") != null) {
+                    supportFragmentManager.beginTransaction().hide(supportFragmentManager.findFragmentByTag("contact")).commit();
+                }
+            } else {
+                if(supportFragmentManager.findFragmentByTag("contact") != null) {
+                    supportFragmentManager.beginTransaction().show(supportFragmentManager.findFragmentByTag("contact")).commit();
+                } else {
+                    supportFragmentManager.beginTransaction().add(layoutReference, fragment, "contact").commit();
+                }
+                if(supportFragmentManager.findFragmentByTag("invest") != null) {
+                    supportFragmentManager.beginTransaction().hide(supportFragmentManager.findFragmentByTag("invest")).commit();
+                }
+            }
+
         }
     }
 }
