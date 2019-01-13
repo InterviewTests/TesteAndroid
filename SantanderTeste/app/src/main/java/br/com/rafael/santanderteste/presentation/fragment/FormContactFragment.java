8828af085e214b825792dfdbba35dd96d1e7049b
rafael <rafael.felipe1989@gmail.com>
2019-01-13 00:17:23 -0200

    Removido e criado novamente o fragment do formulario dinamico

---
+++ SantanderTeste/app/src/main/java/br/com/rafael/santanderteste/presentation/fragment/FormContactFragment.java
@@ -0,0 +1,40 @@
+package br.com.rafael.santanderteste.presentation.fragment;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import br.com.rafael.santanderteste.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FormContactFragment extends Fragment {
+
+
+    public FormContactFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.contact_layout, container, false);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
