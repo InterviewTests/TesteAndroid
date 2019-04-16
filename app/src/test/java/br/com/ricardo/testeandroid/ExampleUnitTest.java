30494b6d12fed8f5e363cc35b3afef7fd9cfb3a1
rickale <ti.ricardo.sousa@gmail.com>
2019-04-16 17:14:10 -0300

    Um teste simples do funcionamento correto (caminho feliz) da classe ContactFragment.

--- app/src/test/java/br/com/ricardo/testeandroid/ExampleUnitTest.java
+++ app/src/test/java/br/com/ricardo/testeandroid/ExampleUnitTest.java
@@ -1,17 +1,35 @@
 package br.com.ricardo.testeandroid;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.junit.Assert.*;
+import br.com.ricardo.testeandroid.view.ContactFragment;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
+
+@RunWith(MockitoJUnitRunner.class)
 public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+
+    @Mock
+    ContactFragment fragment;
+
+
+    @Before
+    public void setUp() throws Exception {
+
+        fragment = new ContactFragment();
+        Assert.assertNotNull(fragment);
+        Assert.assertTrue(true);
     }
+
+    @Test
+    public void test() throws Exception{
+        Assert.assertNotNull(fragment);
+        Assert.assertTrue(true);
+    }
+
+
 }
