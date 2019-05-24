30323ff4d9b518b664f28e7a053df0a8439ddc4e
busqe <busqe.robot@gmail.com>
2019-05-24 01:37:38 -0300

    regras
    regas de formatacao do form contato

--- app/src/main/java/br/banco/services/contact/data/template/FromContact.java
+++ app/src/main/java/br/banco/services/contact/data/template/FromContact.java
@@ -1,6 +1,8 @@
 package br.banco.services.contact.data.template;
 
 /**
+ *  https://floating-mountain-50292.herokuapp.com/cells.json
+ *
  *  REGRAS para validar JSON
  *  Algumas regras podem ser definidas externamente como tamnho do arquivo: field:0
  *  getFile() > compareFile() >
@@ -286,22 +288,81 @@ public class FromContact {
         }
 
 
-
-        public void testeJson(){
-
-            // public  String JSON_STRING ="{'cells':[{'id':1,'type':2,'message':'FRASE:','typefield':null,'hidden':false,'topSpacing':60.0,'show':null,'required':false},{'id':2,'type':1,'message':'NOME','typefield':1,'hidden':false,'topSpacing':35.0,'show':null,'required':true},{'id':4,'type':1,'message':'Email','typefield':3,'hidden':true,'topSpacing':35.0,'show':null,'required':true},{'id':6,'type':1,'message':'Telefone','typefield':'telnumber','hidden':false,'topSpacing':10.0,'show':null,'required':true},{'id':3,'type':4,'message':'MESSAGE','typefield':null,'hidden':false,'topSpacing':35.0,'show':4,'required':false},{'id':7,'type':5,'message':'Enviar','typefield':null,'hidden':false,'topSpacing':10.0,'show':null,'required':true}]}";
-            // setPatch(JSON_STRING); // arquivo externo
-            //this.Patch = getPatch();
-
-        }
-        public void testePref(){
-
-         }
-        public void testeDB(){
-
-        }
+    /**
+     *  Formato de dados do Formulario de contato
+     *
+     */
 
 
+    public static String listContactTemplate[][][] =
+            { //0
+
+                    {
+                            {"id", "1"},
+                            {"type", "2"},
+                            {"message", "TEXTO"},
+                            {"typefield", "null"},
+                            {"hidden", "false"},
+                            {"topSpacing", "60.0"},
+                            {"show", "null"},
+                            {"required", "false"}
+                    }
+                    ,
+                    {
+                            {"id", "2"},
+                            {"type", "1"},
+                            {"message", "Nome"},
+                            {"typefield", "1"},
+                            {"hidden", "false"},
+                            {"topSpacing", "35.0"},
+                            {"show", "null"},
+                            {"required", "true"}
+                    }
+                    ,
+                    {
+                            {"id", "4"},
+                            {"type", "1"},
+                            {"message", "Email"},
+                            {"typefield", "3"},
+                            {"hidden", "true"},
+                            {"topSpacing", "35.0"},
+                            {"show", "null"},
+                            {"required", "true"}
+                    },
+                    {
+                            {"id", "6"},
+                            {"type", "1"},
+                            {"message", "telefone"},
+                            {"typefield", "telnumber"},
+                            {"hidden", "false"},
+                            {"topSpacing", "10.0"},
+                            {"show", "null"},
+                            {"required", "true"}
+                    }
+                    ,
+                    {
+                            {"id", "3"},
+                            {"type", "4"},
+                            {"message", "TEXTO"},
+                            {"typefield", "null"},
+                            {"hidden", "false"},
+                            {"topSpacing", "35.0"},
+                            {"show", "4"},
+                            {"required", "false"}
+                    }
+                    ,
+                    {
+                            {"id", "7"},
+                            {"type", "5"},
+                            {"message", "Enviar"},
+                            {"typefield", "null"},
+                            {"hidden", "false"},
+                            {"topSpacing", "10.0"},
+                            {"show", "null"},
+                            {"required", "true"}
+                    }
+
+            };
 
 
 }
