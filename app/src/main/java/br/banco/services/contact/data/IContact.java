bf68b663b95fedc0be55409d9038b7f611e134c8
busqe <busqe.robot@gmail.com>
2019-05-25 13:07:50 -0300

    Contrato
    Criando uma interface para os carregadores de arquivos

--- app/src/main/java/br/banco/services/contact/data/IContact.java
+++ app/src/main/java/br/banco/services/contact/data/IContact.java
@@ -4,7 +4,19 @@ import android.content.Context;
 
 public interface IContact {
 
-    public String configDesign(Context context);
+    public String loadData(Context context);
+
+
+
+    interface IFileTask{
+        String onLoad(Context c, String folder);
+        boolean onSave(String contentStr, String localDir, String fileName, Context c);
+        String onRead(String localDir, String fileName,  Context c);
+        boolean onClear(String dirName, String fileName, Context c);
+    }
 
 }
+
+
+
 
