964b64d1afc0b469317060c6f636c9a0247b78a0
busqe <busqe.robot@gmail.com>
2019-05-27 13:33:27 -0300

    arquivo de teste
    teste de arquivo

---
+++ app/src/main/java/br/banco/services/contact/domain/Produto.java
@@ -0,0 +1,25 @@
+package br.banco.services.contact.domain;
+
+public class Produto {
+
+
+        public String codigo;
+        public String nome;
+
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
