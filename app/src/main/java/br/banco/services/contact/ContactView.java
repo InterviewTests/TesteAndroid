0aa3ba5fd7bda07fa750252be3bbf460c133ef52
busqe <busqe.robot@gmail.com>
2019-05-31 09:41:49 -0300

    Testes de Dependencias
    Testes de Dependencias

--- app/src/main/java/br/banco/services/contact/ContactView.java
+++ app/src/main/java/br/banco/services/contact/ContactView.java
@@ -7,10 +7,12 @@
 
 package br.banco.services.contact;
 
+ import android.app.Activity;
  import android.content.Context;
  import android.content.Intent;
  import android.content.SharedPreferences;
  import android.graphics.Color;
+ import android.os.Build;
  import android.os.Bundle;
  import android.support.constraint.ConstraintLayout;
  import android.support.v7.app.AppCompatActivity;
@@ -19,6 +21,8 @@ package br.banco.services.contact;
  import android.util.Log;
  import android.view.KeyEvent;
  import android.view.View;
+ import android.view.Window;
+ import android.view.WindowManager;
  import android.widget.Button;
  import android.widget.CheckBox;
  import android.widget.CompoundButton;
@@ -84,19 +88,23 @@ package br.banco.services.contact;
      presenter = new ContactPresenter(this);
 
 
+     //drawStatusBar();
+    // drawFormView();
+     //drawNavigation();
 
-     drawFormView();
-     drawNavigation();
-
-     checkPreferences();
+    // checkPreferences();
 
    // presenter.loadView(option, getApplicationContext());
 
-    checkTyping(vFullName);
-    checkTyping(vEmail);
-    checkTyping(vPhone);
+   // checkTyping(vFullName);
+    //checkTyping(vEmail);
+   // checkTyping(vPhone);
 
  }
+
+
+
+
 
      @Override
      public void onResume() {
@@ -104,13 +112,19 @@ package br.banco.services.contact;
 
      }
 
+
+
      /**
       *
       *  formatacao do form Strings ou Pref
       *
       */
 
-       public boolean checkPreferences(){
+
+
+
+
+     public boolean checkPreferences(){
            boolean pref = false;
 
            formPreferences = new FormPreferences(context);
@@ -119,6 +133,7 @@ package br.banco.services.contact;
 
 
            Map<String,?> map = prefs.getAll();
+
 
           // Log.e(map.get())
           // List<FormPreferences> list = new ArrayList<FormPreferences>(map.g);
@@ -357,6 +372,36 @@ package br.banco.services.contact;
         BTAction1.setOnClickListener(this);
         BTAction2.setOnClickListener(this);
     }
+
+
+    public void drawStatusBar(){
+
+
+            //make translucent statusBar on kitkat devices
+            if (Build.VERSION.SDK_INT >= 19 && Build.VERSION.SDK_INT < 21) {
+                setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, true);
+            }
+            if (Build.VERSION.SDK_INT >= 19) {
+                getWindow().getDecorView().setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+            }
+            //make fully Android Transparent Status bar
+            if (Build.VERSION.SDK_INT >= 21) {
+                setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
+                getWindow().setStatusBarColor(Color.TRANSPARENT);
+            }
+        }
+
+        public static void setWindowFlag(Activity activity, final int bits, boolean on) {
+            Window win = activity.getWindow();
+            WindowManager.LayoutParams winParams = win.getAttributes();
+            if (on) {
+                winParams.flags |= bits;
+            } else {
+                winParams.flags &= ~bits;
+            }
+           // win.setAttributes(winParams);
+        }
 
 
 
